82641e776ffbdb6603fbdcd3b81737e596d69adf
h588512 <54103587+h588512@users.noreply.github.com>
2021-04-23 12:12:10 +0200

    updated static/

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => res.send('Worked'));
+// Static directory 'static' with subdirectiories css, js, and html.
+//app.use(express.static('static'));
+//app.use('/js', express.static(__dirname + '/static/js'));
+
+app.get('/', (req,res) => res.sendFile(__dirname + "/index.html"));
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');

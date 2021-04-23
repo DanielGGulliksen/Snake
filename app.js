c5709e01e3e49079b99045d9dbc28410146b79e9
h588512 <54103587+h588512@users.noreply.github.com>
2021-04-23 13:06:20 +0200

    Set static routes

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 
 // Static directory 'static' with subdirectiories css, js, and html.
-//app.use(express.static('static'));
-//app.use('/js', express.static(__dirname + '/static/js'));
-
-app.get('/', (req,res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static('static/html'))
+app.use("/static/js", express.static('./static/js/'))
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');

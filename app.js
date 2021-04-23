7775ba0240d67410b12b468b5786f038d5174e78
h588512 <54103587+h588512@users.noreply.github.com>
2021-04-23 15:55:36 +0200

    Made css directory and basic login screen

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+var socket = require('socket.io');
+
 const app = express();
 
 // Static directory 'static' with subdirectiories css, js, and html.
 app.use(express.static('static/html'))
 app.use("/static/js", express.static('./static/js/'))
+app.use("/static/css", express.static('./static/css/'))
 
-app.listen(5000, () => {
+var server = app.listen(5000, () => {
     console.log('Listening on port 5000');
+});
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log("sock id:" + socket);
 });
--- static/js/client.js
+++ static/js/client.js
@@ -1,3 +1,25 @@
-function show(){
-    console.log("game.js worked.")
+var socket = io.connect('http://localhost:5000');
+
+const loginScreen = document.getElementById("login");
+const gameScreen = document.getElementById('game');
+
+gameScreen.style.display = "none";
+
+const singleButton = document.getElementById("single");
+const multiButton = document.getElementById("multi");
+
+singleButton.addEventListener('click', newSingleplayer);
+multiButton.addEventListener('click', newMultiplayer);
+
+function newSingleplayer(){
+    start();
+}
+
+function newMultiplayer(){
+    start();
+}
+
+function start(){
+    loginScreen.style.display = "none";
+    gameScreen.style.display = "block";
 }

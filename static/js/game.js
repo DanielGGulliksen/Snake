35204705e321d65f740a940fbc7a8771ada6b20a
karolwen <karoline.wennberg@gmail.com>
2021-04-29 14:38:33 +0200

    move snake

--- static/js/game.js
+++ static/js/game.js
@@ -9,7 +9,7 @@ function main(currentTime) {
 
     lastRenderTime = currentTime;
 
-    //updateSnake()
+    updateSnake()
     drawSnake();
 }
 window.requestAnimationFrame(main);
@@ -20,26 +20,62 @@ const snakeBody = [
     { x: 13, y: 11 }
     ];
 
+
+let segments = 0; 
+
 function drawSnake() {
+    for(i = 0; i < segments; i++) {
+        const seg = document.getElementById("" + i);
+        console.log(seg);
+        if(seg != null) {
+        document.getElementById("" + i).remove(); 
+        segments--;
+        }
+    }
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
+        snakeElement.id = segments;
+        segments++; 
         snakeElement.style.gridRowStart = segment.x;
         snakeElement.style.gridColumnStart = segment.y;
         snakeElement.classList.add('snake'); // <div class="snake"></div>
         gameScreen.appendChild(snakeElement);
+        
     });
 }
 
 function updateSnake() {
-    console.log('update snake');
+    const head = {x: snakeBody[0].x + 1, y: snakeBody[0].y};
+    snakeBody.unshift(head);
+    snakeBody.pop(); 
+    
+    //console.log('update snake');
 }
 
-let direction = { x: 0, y: 0 }
-let previousDirection = { x: 0, y: 0 }
 
-document.addEventListener('keydown', keydown);
 
-function keydown(e) {
+document.addEventListener('keydown', snakeDirection());
+
+function snakeDirection(e) {
+    let direction = { x: 0, y: 0 }
+    
+
+    if (e.keyCode === 37) { //previousDirection.x !== 0
+        direction = { x: -1, y: 0 }
+    }
+    if (e.keyCode === 38 ) { //&& previousDirection.y !== 0
+        direction = { x: 0, y: -1 }
+    }
+    if (e.keyCode === 39 ) { //&& previousDirection.x !== 0
+        direction = { x: 1, y: 0 }
+    }
+    if (e.keyCode === 40 ) { //&& previousDirection.y !== 0
+        direction = { x: 0, y: 1 }
+    }
+
+
+
+    /*
     switch(e.keyCode) {
         case "keyUp" :
             if (previousDirection.y !== 0) break
@@ -57,7 +93,7 @@ function keydown(e) {
             if (previousDirection.x !== 0) break
             direction = { x: -1, y: 0 }
         break;
-    }
+    } */
 }
  
 function getDirection() {
